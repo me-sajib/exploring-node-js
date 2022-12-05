8b3c0a6a9385fabf079bb3450e3bef45d24db247
me-sajib <me.mrsajib@gmail.com>
2022-12-05 13:01:56 +0600

    exploring os & path

--- basic/index.js
+++ basic/index.js
@@ -1,6 +1,22 @@
 const fs = require("fs");
+const os = require("os");
+const path = require("path");
 // const { getName } = require("./students");
 
+// =========== os ============
+// console.log(os.hostname());
+// console.log(os.homedir());
+// console.log(os.totalmem());
+// console.log(os.freemem());
+
+// =========== path ===========
+
+// console.log(path.extname("index.js"));
+// console.log(__dirname);
+// console.log(__filename);
+// console.log(path.extname("index.c"));
+const joinName = path.join(__dirname + "/view");
+console.log(joinName);
 // =========== write file ==============
 // fs.writeFile("delete.txt", "hello world", (err) => {
 //   if (err) {
@@ -41,10 +57,10 @@ const fs = require("fs");
 
 // ============== read file ===========
 
-fs.readFile("test.txt", "utf-8", (err, data) => {
-  if (err) {
-    console.log("something went wrong");
-  } else {
-    console.log(data);
-  }
-});
+// fs.readFile("test.txt", "utf-8", (err, data) => {
+//   if (err) {
+//     console.log("something went wrong");
+//   } else {
+//     console.log(data);
+//   }
+// });
