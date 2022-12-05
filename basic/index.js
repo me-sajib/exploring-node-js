81a8890c7994a4178ed930fdf80b8a2b7a70b05f
me-sajib <me.mrsajib@gmail.com>
2022-12-05 12:46:52 +0600

    exploring file system

--- basic/index.js
+++ basic/index.js
@@ -1,3 +1,50 @@
-const { getName } = require("./students");
+const fs = require("fs");
+// const { getName } = require("./students");
 
-console.log(getName());
+// =========== write file ==============
+// fs.writeFile("delete.txt", "hello world", (err) => {
+//   if (err) {
+//     console.log("something went problem");
+//   } else {
+//     console.log("write file successful");
+//   }
+// });
+
+// ========== append file ===============
+
+// fs.appendFile("demo.txt", " with javascript", (err) => {
+//   if (err) {
+//     console.log("something went wrong");
+//   } else {
+//     console.log("successful");
+//   }
+// });
+
+// ============= rename the file name ===========
+// fs.rename("demo.txt", "test.txt", (err) => {
+//   if (err) {
+//     console.log("something went wrong");
+//   } else {
+//     console.log("rename successful");
+//   }
+// });
+
+// ======= delete file ==============
+
+// fs.unlink("delete.txt", (err) => {
+//   if (err) {
+//     console.log("something went wrong");
+//   } else {
+//     console.log("file deleted");
+//   }
+// });
+
+// ============== read file ===========
+
+fs.readFile("test.txt", "utf-8", (err, data) => {
+  if (err) {
+    console.log("something went wrong");
+  } else {
+    console.log(data);
+  }
+});
